6ca3789868c808b230ba1e7c185586edb5cc7cab
brayanSV <villarragabrayan@hotmail.com>
2021-09-18 21:20:19 -0500

    Ajustes de estilo en el activity de login

--- app/src/main/java/com/user/brayan/pruebatec_todo1/ui/history/History.kt
+++ app/src/main/java/com/user/brayan/pruebatec_todo1/ui/history/History.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.user.brayan.pruebatec_todo1.R
 
-class HistoryActivity : AppCompatActivity() {
+class History : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
